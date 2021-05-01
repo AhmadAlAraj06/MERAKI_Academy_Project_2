7f4b3b29c9757a1c8fb46d9f5bbfbba107697b48
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-05-01 20:13:33 +0300

    last commit

--- git/main.js
+++ git/main.js
@@ -13,6 +13,24 @@ $(document).ready(function(){
   $("#icecreamma").hide();
   $("#drinke").hide();
   $("#waterpr").hide();
+  $("#toffiproduct").hide();
+  $("#tobl11product").hide();
+  $("#tableproduct").hide();
+  $("#bready").hide();
+  $("#skittlesss").hide();
+  $("#mennntt").hide();
+  $("#poppppp").hide();
+  $("#brrrrrrr").hide();
+  $("#tommmm").hide();
+  $("#eggggggg").hide();
+  $("#aaaaaaaaaaa").hide();
+  $("#alammamascd").hide();
+  $("#dsvfzx").hide();
+  $("#fgfgsrgf").hide();
+  $("#fgdvngggg").hide();
+  $("#mhmgmhgmm").hide();
+  $("#tannnnng").hide();
+  $("#nnnne").hide();
 
   
   $("#menue").click(function(){
@@ -167,9 +185,194 @@ $(document).ready(function(){
     $("#prodoption").show();
 
   });
+  $(".tof").click(function(){
+    
+    $("#snackcandy").hide();
+    $("#toffiproduct").show();
 
+  });
+  $("#needstoffi").click(function(){
+    
+    $("#toffiproduct").hide();
+    $("#snackcandy").show();
+  });
+  $(".toblee").click(function(){
+    
+    $("#snackcandy").hide();
+    $("#tableproduct").show();
+    
+  });
+  $("#needstoble").click(function(){
+    
+    $("#tableproduct").hide();
+    $("#snackcandy").show();
+  });
+  $(".bready").click(function(){
+    
+    $("#snackcandy").hide();
+    $("#bready").show();
+    
+  });
+  $("#needsready").click(function(){
+    
+    $("#bready").hide();
+    $("#snackcandy").show();
+  });
+  $(".skitttr").click(function(){
+    
+    $("#snackcandy").hide();
+    $("#skittlesss").show();
+    
+  });
+  $("#needsskit").click(function(){
+    
+    $("#skittlesss").hide();
+    $("#snackcandy").show();
+  });
+  $(".mennn").click(function(){
+    
+    $("#snackcandy").hide();
+    $("#mennntt").show();
+    
+  });
+  $("#needsment").click(function(){
+    
+    $("#mennntt").hide();
+    $("#snackcandy").show();
+  });
+  $(".poppp").click(function(){
+    
+    $("#snackcandy").hide();
+    $("#poppppp").show();
+    
+  });
+  $("#needspopcornnn").click(function(){
+    
+    $("#poppppp").hide();
+    $("#snackcandy").show();
+  });
+  $(".brrr").click(function(){
+    
+    $("#breadindex").hide();
+    $("#brrrrrrr").show();
+    
+  });
+  $("#needsbrbrb").click(function(){
+    
+    $("#brrrrrrr").hide();
+    $("#breadindex").show();
+  });
+  $(".tommm").click(function(){
+    
+    $("#vegetabless").hide();
+    $("#tommmm").show();
+    
+  });
+  $("#needstomoyy").click(function(){
+    
+    $("#tommmm").hide();
+    $("#vegetabless").show();
+  });
+  $(".egggg").click(function(){
+    
+    $("#Dairyeggs").hide();
+    $("#eggggggg").show();
+    
+  });
+  $("#needssssss").click(function(){
+    
+    $("#eggggggg").hide();
+    $("#Dairyeggs").show();
+  });
+  $(".aaaaa").click(function(){
+    
+    $("#Deli").hide();
+    $("#aaaaaaaaaaa").show();
+    
+  });
+  $("#needsaaaaa").click(function(){
+    
+    $("#aaaaaaaaaaa").hide();
+    $("#Deli").show();
+  });
+  $(".alammama").click(function(){
+    
+    $("#milkn").hide();
+    $("#alammamascd").show();
+    
+  });
+  $("#needsallalal").click(function(){
+    
+    $("#alammamascd").hide();
+    $("#milkn").show();
+  });
+  $(".bbbbbbb").click(function(){
+    
+    $("#frooozen").hide();
+    $("#dsvfzx").show();
+    
+  });
+  $("#needsdfrdgfbd").click(function(){
+    
+    $("#dsvfzx").hide();
+    $("#frooozen").show();
+  });
+  $(".ccccccc").click(function(){
+    
+    $("#breakfastc").hide();
+    $("#fgfgsrgf").show();
+    
+  });
+  $("#needsbbrrrrr").click(function(){
+    
+    $("#fgfgsrgf").hide();
+    $("#breakfastc").show();
+  });
+  $(".cofffff").click(function(){
+    
+    $("#coffetea").hide();
+    $("#fgdvngggg").show();
+    
+  });
+  $("#needsccccc").click(function(){
+    
+    $("#fgdvngggg").hide();
+    $("#coffetea").show();
+  });
+  $(".mmmmmm").click(function(){
+    
+    $("#icecreamma").hide();
+    $("#mhmgmhgmm").show();
+    
+  });
+  $("#needsmmmm").click(function(){
+    
+    $("#mhmgmhgmm").hide();
+    $("#icecreamma").show();
+  });
+  $(".tangggggg").click(function(){
+    
+    $("#drinke").hide();
+    $("#tannnnng").show();
+    
+  });
+  $("#needsdfjifd").click(function(){
+    
+    $("#tannnnng").hide();
+    $("#drinke").show();
+  });
+$(".wwwwwwwwwater").click(function(){
+    
+  $("#waterpr").hide();
+  $("#nnnne").show();
+  
 });
-
+$("#needswwwwwwaa").click(function(){
+  
+  $("#nnnne").hide();
+  $("#waterpr").show();
+});
+});
 
 
 
